34c9d3057b00becfd473b0f258667c68c24a1a4f
standroidbeta <43893890+standroidbeta@users.noreply.github.com>
2019-06-03 08:31:50 -0400

    Upated with the generate products function working correctly.

--- acme_report.py
+++ acme_report.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 
-import random
+from random import sample, randint, uniform
+from typing import List, Any, Union
+
 from acme import Product
 
 # Useful to use with random.sample to generate names
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
-def generate_products(num_products=30):
-    import random
-    products = []
-    for n in range(6):
-        ' '.join([NOUNS[random.randint(0, len(NOUNS))] for products in range(1)])
-    return products
+
+def generate_products(num_product=30):
+    """Randomly generates product list"""
+    products = [' '.join(sample(ADJECTIVES, 1) + sample(NOUNS, 1))
+                for _ in range(num_product)]
+    products_set = set(products)
+    products = len(products_set)
+
+    return print('Unique number of products:', products)
 
 
-# def inventory_report(products):
-#     pass  # TODO - your code! Loop over the products to calculate the report.
-#
-#
-# if __name__ == '__main__':
-#     inventory_report(generate_products())
-#
+def inventory_report(products):
+
+    pass
+
+
+if __name__ == '__main__':
+    inventory_report(generate_products())
+
